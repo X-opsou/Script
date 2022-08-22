3acabc96544d32e478c6f99d9712bd2daad7f646
opsou <78749899+X-opsou@users.noreply.github.com>
2022-08-22 16:24:20 +0800

    Update removeAds.js

--- removeAds.js
+++ removeAds.js
@@ -5,6 +5,6 @@ let obj = $response.body
 //var obj1 = obj.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">')
 let newStr = 'style="display:none"'
 var start = obj.search(/div id="index-pop" class="ads"/)
-console.log(start)
+console.log(obj1)
 var obj1 = insertStr(obj,start,newStr)
 $done({body:obj1});
