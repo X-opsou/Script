c723a9f5c109d82ba0ea624e0b23c6cf5a32f469
opsou <78749899+X-opsou@users.noreply.github.com>
2022-08-22 15:50:09 +0800

    Update removeAds.js

--- removeAds.js
+++ removeAds.js
@@ -1,5 +1,5 @@
 let obj = $response.body;
 var null_string = "";
-var obj1 = obj.replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string)
+var obj1 = obj.replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string).replace(/<a href='#'>[\s\S]*?<\/div>/g,null_string)
 console.log(obj1)
 $done({body: obj1});
