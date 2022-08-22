818ffad1ffbd5bce8f8e8de71f92637ac9ff064d
opsou <78749899+X-opsou@users.noreply.github.com>
2022-08-22 16:58:41 +0800

    Update removeAds.js

--- removeAds.js
+++ removeAds.js
@@ -3,6 +3,7 @@ function insertStr(soure, start, newStr){
  }
 let obj = $response.body
 let newStr = 'style="display:none"'
+var null_string = ""
 var start = obj.search(/class="mobileAds"/i)
 var obj1 = insertStr(obj,start,newStr).replace(/<div class="ads text-center"[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string)
 console.log(obj1)
