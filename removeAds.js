a0c9b1e50c005c32e3d569afdce409546fafa6c6
opsou <78749899+X-opsou@users.noreply.github.com>
2022-08-22 16:45:36 +0800

    Update removeAds.js

--- removeAds.js
+++ removeAds.js
@@ -1,10 +1,9 @@
 function insertStr(soure, start, newStr){   
-    return soure.slice(0, start) + newStr + soure.slice(start);
+    return soure.slice(0, start) + " " + newStr + " " + soure.slice(start);
  }
 let obj = $response.body
-//var obj1 = obj.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Code-Dramatist/ss/main/math_css/math.css" type="text/css">')
 let newStr = 'style="display:none"'
-var start = obj.search(/div id="index-pop" class="ads"/)
-console.log(typeof obj)
+var start = obj.search(/div id="index-pop" class="ads"/i)
 var obj1 = insertStr(obj,start,newStr)
+console.log(obj1)
 $done({body:obj1});
