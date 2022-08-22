360a617903492d0d66bd35abf2559020b38e27a0
opsou <78749899+X-opsou@users.noreply.github.com>
2022-08-22 22:31:43 +0800

    Update removeAds.js

--- removeAds.js
+++ removeAds.js
@@ -3,11 +3,7 @@ function insertStr(soure, start, newStr){
  }
 
 let obj = $response.body
-let newStr = 'style="display:none"'
 var null_string = ""
-var start = obj.search(/class="text-center"><script/i)
-var obj1 = insertStr(obj,start,newStr)
-var start1 = obj1.lastIndexOf('class="text-center"><script')
-var obj2 = insertStr(obj1,start1,newStr).replace(/<div class="ads text-center"[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string).replace(/<div id="footer">[\s\S]*?<\/div>/g,null_string).replace(/<div class="pop_bot">[\s\S]*?<\/div>/g,null_string)
+var obj2 = insertStr(obj1,start1,newStr).replace(/<div class="ads text-center"[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string).replace(/<div id="footer">[\s\S]*?<\/div>/g,null_string).replace(/<div class="pop_bot">[\s\S]*?<\/div>/g,null_string).replace(/<div class="text-center">[\s\S]*?<\/div>/g,null_string)
 console.log(obj2)
 $done({body:obj2});
