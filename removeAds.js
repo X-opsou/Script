2da04093c6d60c70c7b44f93be831eeaff1f3ca8
opsou <78749899+X-opsou@users.noreply.github.com>
2022-08-22 17:14:16 +0800

    Update removeAds.js

--- removeAds.js
+++ removeAds.js
@@ -5,6 +5,8 @@ let obj = $response.body
 let newStr = 'style="display:none"'
 var null_string = ""
 var start = obj.search(/class="mobileAds"/i)
-var obj1 = insertStr(obj,start,newStr).replace(/<div class="ads text-center"[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string)
-console.log(obj1)
-$done({body:obj1});
+var obj1 = insertStr(obj,start,newStr)
+var start1 = obj1.search(/class="ads-leaderboard text-center"/i)
+var obj2 = insertStr(obj1,start1,newStr).replace(/<div class="ads text-center"[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string)
+console.log(obj2)
+$done({body:obj2});
