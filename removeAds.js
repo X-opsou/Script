239e67336c440089959d87a190b8b08d1122d5d7
opsou <78749899+X-opsou@users.noreply.github.com>
2022-08-22 17:22:56 +0800

    Update removeAds.js

--- removeAds.js
+++ removeAds.js
@@ -11,5 +11,5 @@ var start1 = obj1.search(/class="ads-leaderboard text-center"/i)
 var obj2 = insertStr(obj1,start1,newStr)
 var start2 = obj2.search(/class="ads text-center"/i)
 var obj3 = insertStr(obj2,start2,newStr).replace(/<div class="ads text-center"[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_keywords container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="col-md-1 col-xs-3">[\s\S]*?<\/div>/g,null_string).replace(/<div class="footer_partners container text-center">[\s\S]*?<\/div>/g,null_string).replace(/<div class="container">[\s\S]*?<\/div>/g,null_string)
-console.log(obj2)
-$done({body:obj2});
+console.log(obj3)
+$done({body:obj3});
