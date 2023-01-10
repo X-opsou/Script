998e2b934c5e254d3bfc974efba9d8547bc34a8e
opsou <78749899+X-opsou@users.noreply.github.com>
2023-01-10 13:27:56 +0800

    Update MyTest.js

--- Stay/MyTest.js
+++ Stay/MyTest.js
@@ -1,4 +1,17 @@
-var aNewBodyElement = document.createElement("body");
-aNewBodyElement.id = "newBodyElement";
-document.body = aNewBodyElement;
-alert(document.body.id); // "newBodyElement"
+// ==UserScript==
+// @name         New Userscript
+// @namespace    https://stay.app/
+// @version      0.1
+// @description  Template userscript created by Stay
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+(function () {
+  'use strict';
+  // Your code here...
+  var aNewBodyElement = document.createElement("body");
+  aNewBodyElement.id = "newBodyElement";
+  document.body = aNewBodyElement;
+  alert(document.body.id); // "newBodyElement"
+})();
